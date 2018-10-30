782b7f98bf16595817c18e910c5920fa6ea65b8f
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-10-30 15:09:35 +0100

    Test

--- app/src/main/java/com/example/maartenvandenhof/studentmenu/Screens/HomeScreen.java
+++ app/src/main/java/com/example/maartenvandenhof/studentmenu/Screens/HomeScreen.java
@@ -2,12 +2,14 @@ package com.example.maartenvandenhof.studentmenu.Screens;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.example.maartenvandenhof.studentmenu.R;
 
@@ -40,4 +42,7 @@ public class HomeScreen extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Button btn = (Button)findViewById(R.menu.drawermenu.);
+
 }
--- app/src/main/java/com/example/maartenvandenhof/studentmenu/Screens/MenuListScreen.java
+++ app/src/main/java/com/example/maartenvandenhof/studentmenu/Screens/MenuListScreen.java
@@ -1,6 +1,23 @@
 package com.example.maartenvandenhof.studentmenu.Screens;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.maartenvandenhof.studentmenu.*;
+
+import java.util.ArrayList;
+
 public class MenuListScreen extends AppCompatActivity {
+    protected ArrayList<Menu> menuList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    public MenuListScreen(){
+        this.menuList = new ArrayList<>();
+
+    }
 }
