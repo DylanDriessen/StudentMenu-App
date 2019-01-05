6a70fff26ed841b4587f318d35dfb0e09f87e867
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2019-01-05 22:43:30 +0100

    edit

--- app/src/main/java/com/example/maartenvandenhof/studentmenu/Activities/MainActivity.java
+++ app/src/main/java/com/example/maartenvandenhof/studentmenu/Activities/MainActivity.java
@@ -54,12 +54,7 @@ import com.example.maartenvandenhof.studentmenu.Fragments.MenuOrdendListFragment
 import com.example.maartenvandenhof.studentmenu.Fragments.MenuPriceSearchFragment;
 import com.example.maartenvandenhof.studentmenu.Fragments.PriceOrdendListFragment;
 import com.example.maartenvandenhof.studentmenu.Fragments.WeekMenuListFragment;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 
 import org.w3c.dom.Text;
 
