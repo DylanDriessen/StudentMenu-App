d1e50642f388e5e6da6df852000e3c8ecc4f8579
Dylan Driessen <dylan.driessen@student.ucll.be>
2019-01-06 11:48:22 +0100

    zoomCamera Google Maps

--- app/src/main/java/com/example/maartenvandenhof/studentmenu/Activities/MainActivity.java
+++ app/src/main/java/com/example/maartenvandenhof/studentmenu/Activities/MainActivity.java
@@ -57,6 +57,10 @@ import com.example.maartenvandenhof.studentmenu.Fragments.PriceOrdendListFragmen
 import com.example.maartenvandenhof.studentmenu.Fragments.WeekMenuListFragment;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -88,6 +92,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public FusedLocationProviderClient mFusedLocationClient;
     public double lat;
     public double lon;
+    public GoogleMap mGoogleMap;
+    public LatLngBounds mMapBoundary;
 
 
     int PERMISSION_ALL = 1;
@@ -702,13 +708,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Log.d(TAG, "getLastKnowLocation: called.");
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
@@ -725,6 +724,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
     }
+
+    /*public void moveCamera(){
+
+        double bottomBoundary = lat -.1;
+        double leftBoundary = lon - .1;
+        double topBoundary = lat + .1;
+        double rightBoundary = lon + .1;
+
+        mMapBoundary = new LatLngBounds(new LatLng(bottomBoundary, leftBoundary), new LatLng(topBoundary, rightBoundary));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mMapBoundary, 0));
+    }*/
 
 
 
